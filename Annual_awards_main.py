28d95d49b5b1bf2d63259a6b95361a54add36e77
ggang.liu <304241378@qq.com>
2018-01-26 12:30:45 +0800

    稳定版本

--- Annual_awards_main.py
+++ Annual_awards_main.py
@@ -6,8 +6,10 @@ import pygame
 import random
 from pygame.locals import *
 
-fullScreen = False
-
+fullScreen = True
+mode = 0
+mouse_scan = [(1031, 498, 116, 89), (1031, 436, 91, 68)]
+pos_scan   = [(683, 151, 683,630), (650, 130, 650, 540)]
 #解析 人员.xlsx 文件，得到人员名单列表
 def get_name_list_from_excel(file_name):
     name_list = []
@@ -30,13 +32,13 @@ def handle_mouse_event(index, pause_flag):
         x, y = pygame.mouse.get_pos()
         x -= m.get_width() / 2
         y -= m.get_height() / 2
-        if 1031 + 91 > x > 1031 and 436 + 68 > y > 436:
+        if mouse_scan[mode][0] + mouse_scan[mode][2] > x > mouse_scan[mode][0] and mouse_scan[mode][1] + mouse_scan[mode][3] > y > mouse_scan[mode][1]:
             pause_flag = True if pause_flag == False else False
             if not pause_flag:
                 pygame.mixer.music.play()
                 del name_list[index]
                 print len(name_list)
-                show_name_list(name_list)
+                #show_name_list(name_list)
             else:
                 pygame.mixer.music.stop()
 
@@ -47,21 +49,28 @@ def show_name_list(name_list):
     '''调试接口，输出当前名单'''
     for index in range(0, len(name_list)):
         str = "%s %s" % (name_list[index][0], name_list[index][1])
-        print(str.decode('utf-8'))
+        print(str)
+        #print(str.decode('utf-8'))
 
 if __name__ == "__main__":
     job_num, job_name, name_list = get_name_list_from_excel(r'name_file.xlsx')
     print len(name_list)
 
     pygame.init()
+    bg = 'bg.png'
+    mg = 'gc_cz.png'
+
     if fullScreen:
-        screen = pygame.display.set_mode((1024, 768), FULLSCREEN, 32)
+        mode = 0
+        bg = 'bg_1366x768.png'
+        screen = pygame.display.set_mode((1366, 768), FULLSCREEN, 32)
     else:
+        mode = 1
+        bg = 'bg.png'
         screen = pygame.display.set_mode((1340, 670), 0, 32)
 
     pygame.display.set_caption("文思海辉年会抽奖程序")
-    bg = 'bg.png'
-    mg = 'cz.png'
+
 
     #newimg = pygame.transform.scale(bg, (1024, 480))
 
@@ -81,6 +90,7 @@ if __name__ == "__main__":
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                 pygame.quit()
+                sys.exit(0)
             elif event.type == MOUSEBUTTONDOWN:
                 pause_flag = handle_mouse_event(index, pause_flag)
 
@@ -93,19 +103,21 @@ if __name__ == "__main__":
 
         if not pause_flag:
             index = random.randint(0, len(name_list)-1)
+            if pygame.mixer.music.get_busy() == False:
+                pygame.mixer.music.play()
 
         text_context = '%s %s' % (name_list[index][0], name_list[index][1])
         #print text_context
-        font = pygame.font.SysFont("SimHei", 60)
+        font = pygame.font.Font("simhei.ttf", 65)
         text_obj = font.render(text_context.decode('utf-8'), True, (255, 255, 255), (0, 0, 0))
         text_pos = text_obj.get_rect()
-        text_pos.center = (650, 130)
+        text_pos.center = (pos_scan[mode][0], pos_scan[mode][1])
         screen.blit(text_obj, text_pos)
 
-        font = pygame.font.SysFont("SimHei", 30)
-        text_obj = font.render('Ericsson ODC', True, (255, 255, 255), (255, 0, 0))
+        font = pygame.font.Font("simhei.ttf", 30)
+        text_obj = font.render('Ericsson ODC 祝大家鸿运'.decode('utf-8'), True, (255, 255, 255), (255, 0, 0))
         text_pos = text_obj.get_rect()
-        text_pos.center = (650, 540)
+        text_pos.center = (pos_scan[mode][2], pos_scan[mode][3])
         screen.blit(text_obj, text_pos)
 
         pygame.display.update()
