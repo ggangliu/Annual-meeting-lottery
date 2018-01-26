df4801f777fa32524b3bb44ca997cf6a5f8c49c0
ggang.liu <304241378@qq.com>
2018-01-26 14:19:57 +0800

    修改打包脚本

--- convert_to_exe.py
+++ convert_to_exe.py
@@ -2,7 +2,8 @@ import os
 
 os.system("pyinstaller -F Annual_awards_main.py")
 os.system("copy 9224.wav dist\\")
+os.system("copy bg_1366x768.png dist\\")
 os.system("copy bg.png dist\\")
-os.system("copy cz.png dist\\")
+os.system("copy gc_cz.png dist\\")
 os.system("copy name_file.xlsx dist\\")
 os.system("copy simhei.ttf dist\\")
